037b2450bfd72f55e9dce861b6bceb524128bf1c
Jhonnatha de Andrade Monteiro <jhonnatha_am@jhonn.local>
2020-03-21 23:16:19 -0300

    160

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {...};
+  // }
+  state = {
+    ingredients:{
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Burger/>
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </React.Fragment>
     )
