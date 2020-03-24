7bf5effb7cadd81315c1d23ea97146bd521add0a
Jhonnatha de Andrade Monteiro <jhonnatha_am@jhonn.local>
2020-03-23 23:04:41 -0300

    buildcontrols styling

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+
+const BuildControls = (props) => {
+  return (
+    <div className={...classes}>
+      
+    </div>
+  )
+}
+
+export default BuildControls;
